03a32ec91bc92bac23a97c2360439a7f01690d01
Dacarpe03 <d.carmonap97@gmail.com>
2023-01-12 18:50:00 +0100

    Create script

---
+++ qa_system.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 18:28:30 2023
+
+@author: Daniel
+"""
+
+QUESTIONS_PREFIX = "Preguntas/"
+DIMENSIONS = [""]
+
+def main():
+    
+
+def load_questions():
+    questions
+    with open(
+    
+def load_dictionaries():
+    return
+
+if __name__ == "__main__":
+    main()
