0cf0c0b29c5fabe47bb2ad928f17f2eaf921b9ab
Dacarpe03 <d.carmonap97@gmail.com>
2023-01-13 00:12:53 +0100

    Add a dictionary of pronouns

--- qa_system.py
+++ qa_system.py
@@ -7,17 +7,18 @@ Created on Thu Jan 12 18:28:30 2023
 import pandas as pd
 
 QUESTIONS_PREFIX = "Preguntas/preguntas_"
-DIMENSIONS = ["nombre", 
-              "primer_apellido", 
-              "segundo_apellido", 
-              "lugar_de_nacimiento", 
-              "dia_de_nacimiento", 
-              "mes_de_nacimiento", 
+PRONOUNS_FILE = "Pronombres/pronombres.txt"
+DIMENSIONS = ["nombre",
+              "primer_apellido",
+              "segundo_apellido",
+              "lugar_de_nacimiento",
+              "dia_de_nacimiento",
+              "mes_de_nacimiento",
               "anno_de_nacimiento",
               "fecha_de_nacimiento",
               "edad",
-              "dia_de_fallecimiento", 
-              "mes_de_fallecimiento", 
+              "dia_de_fallecimiento",
+              "mes_de_fallecimiento",
               "anno_de_fallecimiento",
               "fecha_de_fallecimiento",
               "marido",
@@ -31,26 +32,33 @@ DIMENSIONS = ["nombre",
               "puestos_empresariales"
               ]
 
-PRONOUNS_LIST = ["cómo",
-                 "cuál",
-                 "cuán",
-                 "cuándo",
-                 "cuánto",
-                 "dónde",
-                 "qué",
-                 "quién"]
-
-
 
 def main():
-    weights_dictionary = create_weights_dictionary()
-    calculate_pronoun_weights(weights_dictionary)
+    pronouns_dict = get_dictionary(PRONOUNS_FILE)
+    print(pronouns_dict)
+    weights_dictionary = create_weights_dictionary(pronouns_dict)
+    
+    calculate_pronoun_weights(weights_dictionary, 
+                              pronouns_dict)
+    
+    print(weights_dictionary)
     df = pd.DataFrame(weights_dictionary)
     df.to_csv('prueba.csv')
+    
 
-def create_weights_dictionary():
+def get_dictionary(dictionary_file_name):
+    pronouns_dict = {}
+    with open(dictionary_file_name, "r", encoding="utf-8") as pron_file:
+        for pron in pron_file.readlines():
+            pron = pron.replace("\n","")
+            key, values = pron.split(":")
+            pronouns_dict[key] = values.split(",")
+    return pronouns_dict
+
+
+def create_weights_dictionary(pronouns_dict):
     weights_dictionary = {"palabra": []}
-    for pron in PRONOUNS_LIST:
+    for pron in pronouns_dict.keys():
         weights_dictionary["palabra"].append(pron)
         
     for dimension in DIMENSIONS:
@@ -58,28 +66,28 @@ def create_weights_dictionary():
     return weights_dictionary
 
 
-def calculate_pronoun_weights(weights_dictionary):
-    for pron in PRONOUNS_LIST:
+def calculate_pronoun_weights(weights_dictionary, pronouns_dict):
+    for pron in pronouns_dict.keys():
         for dimension_name in DIMENSIONS:
             pron_count = 0
             questions_list = load_questions(dimension_name)
             for question in questions_list:
-                pronoun_found = find_pronoun(question)
-                if pronoun_found == pron:
+                if check_question_pronoun(question, pron, pronouns_dict):
                     pron_count+=1
             weight = pron_count/len(questions_list)
             weights_dictionary[dimension_name].append(weight)
+            
 
-
-def find_pronoun(question):
-    processed_sentence = question.lower()
-    processed_sentence = processed_sentence.replace("¿", "")
-    processed_sentence = processed_sentence.replace("?", "")
-    for word in processed_sentence.split(" "):
-        if word in PRONOUNS_LIST:
-            return word
-    return None
-
+def check_question_pronoun(question, pronoun_key, pronouns_dict):
+    question_processed = question.lower()
+    question_processed = question_processed.replace("?","")
+    question_processed = question_processed.replace("¿","")
+    
+    for word in question_processed.split(" "):
+        print(word)
+        if word in pronouns_dict[pronoun_key]:
+            return True
+    return False
     
 def load_questions(dimension_name):
     question_filename = f"{QUESTIONS_PREFIX}{dimension_name}.txt"
@@ -87,10 +95,6 @@ def load_questions(dimension_name):
     with open(question_filename, "r", encoding='utf-8') as questions:
         lines = questions.readlines()
     return lines
-          
-
-def load_dictionaries():
-    return
 
 
 if __name__ == "__main__":
