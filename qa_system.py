f21f79c08f32277a1b46cdf7ed34317cced82a2d
Dacarpe03 <d.carmonap97@gmail.com>
2023-01-12 21:07:54 +0100

    Create dataframe with weights

--- qa_system.py
+++ qa_system.py
@@ -4,6 +4,7 @@ Created on Thu Jan 12 18:28:30 2023
 
 @author: Daniel
 """
+import pandas as pd
 
 QUESTIONS_PREFIX = "Preguntas/preguntas_"
 DIMENSIONS = ["nombre", 
@@ -30,23 +31,67 @@ DIMENSIONS = ["nombre",
               "puestos_empresariales"
               ]
 
+PRONOUNS_LIST = ["cómo",
+                 "cuál",
+                 "cuán",
+                 "cuándo",
+                 "cuánto",
+                 "dónde",
+                 "qué",
+                 "quién"]
+
+
+
 def main():
-    create_dictionaries()
+    weights_dictionary = create_weights_dictionary()
+    calculate_pronoun_weights(weights_dictionary)
+    df = pd.DataFrame(weights_dictionary)
+    df.to_csv('prueba.csv')
+
+def create_weights_dictionary():
+    weights_dictionary = {"palabra": []}
+    for pron in PRONOUNS_LIST:
+        weights_dictionary["palabra"].append(pron)
+        
+    for dimension in DIMENSIONS:
+        weights_dictionary[dimension] = []
+    return weights_dictionary
 
 
-def create_dictionaries():
-    for dimension_name in DIMENSIONS:
-        load_questions()
+def calculate_pronoun_weights(weights_dictionary):
+    for pron in PRONOUNS_LIST:
+        for dimension_name in DIMENSIONS:
+            pron_count = 0
+            questions_list = load_questions(dimension_name)
+            for question in questions_list:
+                pronoun_found = find_pronoun(question)
+                if pronoun_found == pron:
+                    pron_count+=1
+            weight = pron_count/len(questions_list)
+            weights_dictionary[dimension_name].append(weight)
+
+
+def find_pronoun(question):
+    processed_sentence = question.lower()
+    processed_sentence = processed_sentence.replace("¿", "")
+    processed_sentence = processed_sentence.replace("?", "")
+    for word in processed_sentence.split(" "):
+        if word in PRONOUNS_LIST:
+            return word
+    return None
+
     
 def load_questions(dimension_name):
-    print("a")
     question_filename = f"{QUESTIONS_PREFIX}{dimension_name}.txt"
+    lines = []
     with open(question_filename, "r", encoding='utf-8') as questions:
-        for line in questions.readlines():
-            print(line)
-              
+        lines = questions.readlines()
+    return lines
+          
+
 def load_dictionaries():
     return
+
 
 if __name__ == "__main__":
     main()
