24788798ea91ef9d372e7fdd919cc23618d85f60
Dacarpe03 <d.carmonap97@gmail.com>
2023-01-13 17:00:27 +0100

    Add code to show answers

--- qa_system.py
+++ qa_system.py
@@ -17,30 +17,58 @@ VERBS_FORMS_FILE = f"{VERBS_PREFIX}formas_verbos.txt"
 KEYWORDS_PREFIX = "PalabrasClave/"
 KEYWORDS_FORMS_FILE = f"{KEYWORDS_PREFIX}formas_palabras_clave.txt"
 
-ELON_ANSWERS = 
+ELON_ANSWERS = "Respuestas/respuestas_elon.txt"
+STEVE_ANSWERS = "Respuestas/respuestas_steve.txt"
+
 
 def main():
     weights_df = load_dataframe()
-    pronouns_dict, verbs_dict, keywords_dict, elon_dict = load_dictionaries()
-    
+    pronouns_dict, verbs_dict, keywords_dict = load_words_dictionaries()
+    elon_dict = get_dictionary(ELON_ANSWERS)
+    steve_dict = get_dictionary(STEVE_ANSWERS)
+    personalities_dict = {"elon":elon_dict, "steve":steve_dict}
     dimensions = weights_df.columns
     n_dimensions = len(dimensions)
     while True:
         question = get_question()
-        scores_array = get_question_scores(question, 
+        personality = detect_personality(question)
+        if personality in personalities_dict.keys():
+            scores_array = get_question_scores(question, 
                                            weights_df, 
                                            n_dimensions,
                                            pronouns_dict,
                                            verbs_dict,
                                            keywords_dict)
-        show_answers(scores_array, dimensions)
+            show_answers(scores_array,
+                         dimensions, 
+                         personalities_dict[personality])
         
         
 def get_question():
-    question = input("Por favor, introduce una pregunta acerca de Elon Musk o Steve Jobs:")
+    question = input("Por favor, introduce una pregunta acerca de Elon Musk o Steve Jobs:\n")
     question = clear_question(question)
     return question
 
+
+def detect_personality(question):
+    has_elon = False
+    has_steve = False
+    for word in question.split(" "):
+        if word in ["elon", "musk"]:
+            has_elon = True
+        if word in ["steve", "jobs"]:
+            has_steve = True
+    
+    if has_elon and has_steve:
+        print("No reconocemos a la personalidad por la que pregunta, por favor, pregunte acerca de Elon Musk o Steve Jobs")
+        return ""
+    elif has_elon:
+        return "elon"
+    elif has_steve:
+        return "steve"
+    else:
+        print("No reconocemos a ninguna personalidad en la pregunta, por favor pregunte acerca de Elon Musk o Steve Jobs")
+        return ""
 
 def get_question_scores(question, 
                         weights_df,
@@ -84,11 +112,16 @@ def get_word_score(word, weights_df):
         return np.zeros(len(weights_df.columns))
     
 
-def show_answers(scores, dimensions):
+def show_answers(scores, dimensions, answers_dict):
     indexes = np.flip(np.argsort(scores))
-    for i in indexes:
-        print(dimensions[i])
-    
+    print("Respuestas de mayor a menor probabilidad (sólo se muestran 3):")
+    for i in range(0,3):
+        dim_index = indexes[i]
+        dim = dimensions[dim_index]
+        concat = ",".join(answers_dict[dim])
+        answer = f"    Dimensión {dim}: {concat}"
+        print(answer)
+
     
 def clear_question(question):
     question_processed = question.lower()
@@ -105,7 +138,7 @@ def load_dataframe():
     return weights_dataframe
 
 
-def load_dictionaries():
+def load_words_dictionaries():
     pronouns_dict = get_dictionary(PRONOUNS_FILE)
     verbs_dict = get_dictionary(VERBS_FORMS_FILE)
     keywords_dict = get_dictionary(KEYWORDS_FORMS_FILE)
